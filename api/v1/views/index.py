9d925335b7f5d5c7bb8153296c6a822cceafe6b5
ckihereko <102221386+ckihereko@users.noreply.github.com>
2022-11-19 15:55:53 +0300

    fix pep8

---
+++ api/v1/views/index.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" index.py"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route("/status")
+def status_ok():
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def obj_stats():
+    objs = {"amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")}
+    return jsonify(objs)
